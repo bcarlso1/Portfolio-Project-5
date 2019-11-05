c663435006bbf33c3f92dc0b7cb394660fee5f24
bcarlso1 <bcarlso1@gmail.com>
2019-11-05 11:59:48 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
-$('data-fancybox="gallery"').fancybox({
-    // Options will go here
+const searchBar = $('#search-bar');
+const images = $('a');
+
+searchBar.keyup(function() {
+    let searchTerm = searchBar.val().toLowerCase();
+        images.show();
+        if (searchTerm !== '') {
+            images.not('[data-caption*="' + searchTerm + '"]').hide();
+        }
+    }
+);
+
+$('[data-fancybox="gallery"]').fancybox({
     loop: 'true',
     transitionEffect: 'slide'
 });
+
