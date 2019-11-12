7747c1d65a3cce7382f2533c2ac2261a5230f9b4
bcarlso1 <bcarlso1@gmail.com>
2019-11-12 11:48:30 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,15 +1,27 @@
-const searchBar = $('#search-bar');
-const images = $('a');
+var entry = document.getElementById('search-bar');
+var items = document.getElementsByTagName('a');
+var itemsParent = document.getElementsByClassName('flex-item')
 
-searchBar.keyup(function() {
-    let searchTerm = searchBar.val().toLowerCase();
-    images.filter(function ( index )
-        {
-            $(this).toggle($(this).attr('data-caption').toLowerCase().indexOf(searchTerm) > -1);
+entry.addEventListener('keyup', myFunction);
+
+function myFunction() {
+    entry.value = entry.value.toLowerCase();
+    var myEntry = entry.value;
+    console.log(myEntry);
+
+    for (var i = 0; i < items.length; i += 1) {
+        var title = items[i].getAttribute('data-caption');
+        console.log(title);
+        
+     // fix this last part, currently hides with any entry bc 1 and 1 is 2.   
+        if (title.indexOf(myEntry) > -1) {
+            itemsParent[i].style.display = "";
+        } else {
+            itemsParent[i].style.display = "none";
         }
-    );
 }
-);
+}
+
 
 $('[data-fancybox="gallery"]').fancybox({
     loop: 'true',
